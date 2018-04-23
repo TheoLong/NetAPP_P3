e49272dba8b9d513a85a2088fb299a53b31b075a
TheoLong <theoloong@gmail.com>
2018-04-22 21:28:17 -0400

    added debug

--- led.py
+++ led.py
@@ -2,7 +2,7 @@
 # @Author: TheoLong
 # @Date:   2018-04-15 00:38:15
 # @Last Modified by:   TheoLong
-# @Last Modified time: 2018-04-22 21:27:38
+# @Last Modified time: 2018-04-22 21:28:07
 import RPi.GPIO as GPIO
 from led_pins import led_pins
 import time
@@ -159,7 +159,7 @@ def updateLED():
             #sleep to rate
             print (current_state)
             time.sleep(sleep_rate)
-            if rdiff ==0 and gdiff ==0 and bdiff ==0
+            if rdiff ==0 and gdiff ==0 and bdiff ==0:
                 break
         else:
 
